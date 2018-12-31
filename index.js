f8f85a33a58430ceef53b2e74667afa8e244c033
abejide001 <abejidefemi1@gmail.com>
2018-12-31 19:54:04 +0100

    [chore] Add middlewares to index.js

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 require('./db_config/database');
+const post = require('./routes/api/post');
+const profile = require('./routes/api/profile');
+const user = require('./routes/api/user');
 
 const app = express();
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/api/post', post);
+app.use('/api/profile', profile);
+app.use('/api/user', user);
 app.get('/', (req, res) => res.send('Hello, welcome to dev'));
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 6003;
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`App listening on ${port}`);
 });
