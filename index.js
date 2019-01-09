c32879d6c468a897e769e5db5382aa1f36151222
abejide001 <abejidefemi1@gmail.com>
2019-01-09 23:12:51 +0100

    Prepare app for deployment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors')
+const cors = require('cors');
+const path = require('path');
 require('./db_config/database');
 const post = require('./routes/api/post');
 const profile = require('./routes/api/profile');
@@ -14,6 +15,14 @@ app.use('/api/post', post);
 app.use('/api/profile', profile);
 app.use('/api/user', user);
 app.get('/', (req, res) => res.send('Hello, welcome to dev'));
+
+// serve static file
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 const port = process.env.PORT || 6003;
 app.listen(port, () => {
   // eslint-disable-next-line no-console
